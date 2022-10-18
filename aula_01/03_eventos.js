2023ed1c27a42b60c52fdcedbcb5c2ec47b86922
joaoelias1921 <joao.reis@aluno.unc.br>
2022-10-18 13:41:30 -0300

    Alterações aula 02

--- aula_01/03_eventos.js
+++ aula_01/03_eventos.js
@@ -21,6 +21,59 @@ bot.start(ctx => {
     `);
 });
 
+// tratando eventos de texto
+bot.on("text", ctx => {
+    const texto = ctx.update.message.text;
+    console.log(texto);
+    ctx.reply(`O texto recebido foi: '${texto}'`);
+})
+
+// tratando eventos de localização
+bot.on("location", ctx => {
+    const loc = ctx.update.message.location;
+    console.log(loc);
+    ctx.reply(`OK! Você está em:
+        Latitude: ${loc.latitude},
+        Longitude: ${loc.longitude}
+    `);
+});
+
+// tratando eventos de contatos
+bot.on("contact", ctx => {
+    const cont = ctx.update.message.contact;
+    console.log(cont);
+    ctx.reply(`Legal! O telefone do ${cont.first_name}
+        é: ${cont.phone_number}
+    `)
+});
+
+// tratando eventos de áudio
+bot.on("voice", ctx => {
+    const voz = ctx.update.message.voice;
+    console.log(voz);
+    ctx.reply(`Áudio de ${voz.duration} segundos!`);
+});
+
+// tratando eventos de imagem/foto
+bot.on("photo", ctx => {
+    const foto = ctx.update.message.photo;
+    console.log(foto);
+    console.log(foto.length);
+    foto.forEach((photo, i) => {
+        ctx.reply(`A ${i}ª foto tem resolução de:
+            ${photo.width} x ${photo.height}
+            pixels!`);
+    });
+});
+
+// tratando eventos de 'stickers'
+bot.on("sticker", ctx =>{
+    const stick = ctx.update.message.sticker;
+    console.log(stick);
+    ctx.reply(`Você enviou o ${stick.emoji}
+        do conjunto ${stick.set_name}`);
+});
+
 // Iniciando o "polling" com o servidor para verificar se há
 // novas mensagens e/ou conversas
 bot.startPolling();
